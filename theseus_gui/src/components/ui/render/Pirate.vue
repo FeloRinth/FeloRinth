bbcd52c1db9a0cc30399e840f003d2f6e7370561
DIDIRUS4 <DIDIRUS@yandex.ru>
2024-01-09 20:57:56 +0300

    [beta] Design update. Bump patch to v17. Update check in Settings impl.

---
+++ theseus_gui/src/components/ui/render/Pirate.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img src="./vectors/pirate.svg" alt="Pirate Icon" class="icon">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PirateIcon'
+};
+</script>
+
+<style>
+.icon {
+  width: 0.85rem;
+  //height: 0.85rem;
+  display: flex;
+  margin-right: 0.5rem;
+}
+</style>
---
+++ theseus_gui/src/components/ui/render/PirateShip.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img src="./vectors/pirate_ship.svg" alt="Pirate Ship Icon" class="icon">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PirateShipIcon'
+};
+</script>
+
+<style>
+.icon {
+  width: 0.85rem;
+  //height: 0.85rem;
+  display: flex;
+  margin-right: 0.5rem;
+}
+</style>
