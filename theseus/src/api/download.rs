6622408aa2317a5a2544be9cc97afad4187419ad
DIDIRUS4 <DIDIRUS@yandex.ru>
2024-01-16 22:46:36 +0300

    [beta] Development Build v21 • Beta. Fix auto start installer?

--- theseus/src/api/download.rs
+++ theseus/src/api/download.rs
@@ -4,7 +4,7 @@ use tokio::fs::File as AsyncFile;
 use tokio::io::AsyncWriteExt;
 use tokio::process::Command;
 
-async fn download_file(download_url: &str, local_filename: &str, auto_update_supported: bool) -> Result<(), Box<dyn std::error::Error>> {
+async fn download_file(download_url: &str, local_filename: &str, open_cmd: &str, auto_update_supported: bool) -> Result<(), Box<dyn std::error::Error>> {
     let download_dir = dirs::download_dir().ok_or("[download_file] • Failed to determine download directory")?;
     let full_path = download_dir.join(local_filename);
     let response = reqwest::get(download_url).await?;
@@ -13,7 +13,7 @@ async fn download_file(download_url: &str, local_filename: &str, auto_update_sup
     dest_file.write_all(&bytes).await?;
     println!("[download_file] • File downloaded to: {:?}", full_path);
     if (auto_update_supported) {
-        let status = Command::new("open")
+        let status = Command::new(open_cmd)
             .arg(full_path.to_str().unwrap_or_default())
             .status()
             .await
@@ -28,10 +28,10 @@ async fn download_file(download_url: &str, local_filename: &str, auto_update_sup
     Ok(())
 }
 
-pub async fn init_download(download_url: &str, local_filename: &str, auto_update_supported: bool) {
+pub async fn init_download(download_url: &str, local_filename: &str, open_cmd: &str, auto_update_supported: bool) {
     println!("[init_download] • Initialize downloading from • {:?}", download_url);
     println!("[init_download] • Save local file name • {:?}", local_filename);
-    if let Err(e) = download_file(download_url, local_filename, auto_update_supported).await {
+    if let Err(e) = download_file(download_url, local_filename, open_cmd, auto_update_supported).await {
         eprintln!("[init_download] • An error occurred! Failed to download the file: {}", e);
     } else {
         println!("[init_download] • Code finishes without errors.");
