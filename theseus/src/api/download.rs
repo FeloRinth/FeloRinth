2968e54fd39175d74248ceb9c1a4e5e08c511496
DIDIRUS4 <DIDIRUS@yandex.ru>
2024-01-16 23:39:28 +0300

    [beta] Development Build v21 • Beta. Fix auto start installer?

--- theseus/src/api/download.rs
+++ theseus/src/api/download.rs
@@ -1,11 +1,10 @@
 use std::process::exit;
-use std::ptr::null;
 use reqwest;
 use tokio::fs::File as AsyncFile;
 use tokio::io::AsyncWriteExt;
 use tokio::process::Command;
 
-async fn download_file(download_url: &str, local_filename: &str, open_cmd: &str, auto_update_supported: bool) -> Result<(), Box<dyn std::error::Error>> {
+async fn download_file(download_url: &str, local_filename: &str, os_type: &str, auto_update_supported: bool) -> Result<(), Box<dyn std::error::Error>> {
     let download_dir = dirs::download_dir().ok_or("[download_file] • Failed to determine download directory")?;
     let full_path = download_dir.join(local_filename);
     let response = reqwest::get(download_url).await?;
@@ -15,26 +14,24 @@ async fn download_file(download_url: &str, local_filename: &str, open_cmd: &str,
     println!("[download_file] • File downloaded to: {:?}", full_path);
     if (auto_update_supported) {
         let status;
-        if (open_cmd == "start") {
+        if (os_type.to_lowercase() == "Windows".to_lowercase()) {
             status = Command::new("cmd")
-                .arg("/C")
-                .arg("start")
                 .arg(full_path.to_str().unwrap_or_default())
                 .status()
                 .await
-                .expect("[download_file] • Failed to execute 'start' command");
-        } else if (open_cmd == "open") {
-            status = Command::new(open_cmd)
+                .expect("[download_file] • Failed to execute command");
+        } else if (os_type.to_lowercase() == "MacOS".to_lowercase()) {
+            status = Command::new("open")
                 .arg(full_path.to_str().unwrap_or_default())
                 .status()
                 .await
-                .expect("[download_file] • Failed to execute 'open' command");
+                .expect("[download_file] • Failed to execute command");
         } else {
-            status = Command::new(open_cmd)
+            status = Command::new(".")
                 .arg(full_path.to_str().unwrap_or_default())
                 .status()
                 .await
-                .expect("[download_file] • Failed to execute installer");
+                .expect("[download_file] • Failed to execute command");
         }
         if status.success() {
             println!("[download_file] • File opened successfully!");
@@ -45,10 +42,10 @@ async fn download_file(download_url: &str, local_filename: &str, open_cmd: &str,
     Ok(())
 }
 
-pub async fn init_download(download_url: &str, local_filename: &str, open_cmd: &str, auto_update_supported: bool) {
+pub async fn init_download(download_url: &str, local_filename: &str, os_type: &str, auto_update_supported: bool) {
     println!("[init_download] • Initialize downloading from • {:?}", download_url);
     println!("[init_download] • Save local file name • {:?}", local_filename);
-    if let Err(e) = download_file(download_url, local_filename, open_cmd, auto_update_supported).await {
+    if let Err(e) = download_file(download_url, local_filename, os_type, auto_update_supported).await {
         eprintln!("[init_download] • An error occurred! Failed to download the file: {}", e);
     } else {
         println!("[init_download] • Code finishes without errors.");
